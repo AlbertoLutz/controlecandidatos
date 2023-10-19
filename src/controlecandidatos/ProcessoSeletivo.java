31fb2bfdd765bc3a588ed0cf8de5f4e223eda5a7
AlbertoLutz <albertolutzdias@gmail.com>
2023-10-18 21:24:31 -0300

    Criando classes

---
+++ src/controlecandidatos/ProcessoSeletivo.java
@@ -0,0 +1,8 @@
+package controlecandidatos;
+
+public class ProcessoSeletivo {
+public static void main(String[] args) {
+	System.out.println("Processo Seletivo");
+}
+static void analisarCandidato()
+}
